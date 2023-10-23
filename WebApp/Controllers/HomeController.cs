a9f43c79e8a009776fb3523cadfa296980799ba1
CyberGriffin <griffinbeaudreau@gmail.com>
2023-10-23 16:08:29 -0500

    Initial MVC environment setup with example login page

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    // Added IActionResult for Login
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    // Added IActionResult for login input
+    [HttpPost]
+    public IActionResult Login(string username, string password) {
+        if (username == "admin" && password == "admin") {
+            return RedirectToAction("Index", "Home");
+        } else if (username == "user" && password == "user") {
+            return RedirectToAction("Privacy", "Home");
+        } else {
+            return RedirectToAction("Login", "Home");
+        }
+    }
+}
