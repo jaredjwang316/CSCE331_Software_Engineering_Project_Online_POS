2ac3c5a3c195654d292cac62f5f4d32e09d93db4
CyberGriffin <griffinbeaudreau@gmail.com>
2023-10-31 00:40:47 -0500

    Implemented OAuth for google accounts. -Includes: new AccountController, modified Home/Index page, Config class, and program.cs

--- WebApp/Config.cs
+++ WebApp/Config.cs
@@ -1,15 +1,29 @@
 /*
-
+    File: Config.cs
+    Author: Griffin Beaudreau
+    Date: October 30, 2023
+    Description: This file contains the Config class, which is used to store
+        general configuration information for the web app.
 */
 
 namespace WebApp;
 
+/// <summary>
+/// Config class, used to store general configuration information.
+/// </summary>
 static class Config {
     public static readonly string CONNECTION_STRING;
     public static readonly string AWS_DB_NAME = "csce315331_07r_db";
+    public static string returnUrl; // This is the URI that the user will be redirected to after logging in.
 
     static Config() {
         CONNECTION_STRING = Environment.GetEnvironmentVariable("POSTGRESQLCONNSTR_AWS_DB")
             ?? throw new ArgumentException("POSTGRESQLCONNSTR_AWS_DB environment variable cannot be null or empty.");
+
+        #if DEBUG
+        returnUrl = "https://localhost:5001";
+        #else
+        returnUrl = "https://07r-webapp.azurewebsites.net";
+        #endif
     }
 }
--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,7 +1,30 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using WebApp;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSingleton(Config.returnUrl);
+builder.WebHost.UseUrls(Config.returnUrl);
+
+
+// Set up Google Authentication scheme.
+builder.Services.AddAuthentication(options => {
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+})
+.AddCookie()
+.AddGoogle(GoogleDefaults.AuthenticationScheme, options =>
+{
+    options.ClientId = configuration["GOOGLE_PROVIDER_AUTHENTICATION_CLIENT_ID"]
+        ?? throw new ArgumentException("Google client id environment variable cannot be null or empty.");
+    options.ClientSecret = configuration["GOOGLE_PROVIDER_AUTHENTICATION_SECRET"]
+        ?? throw new ArgumentException("Google client secret environment variable cannot be null or empty.");
+});
 
 var app = builder.Build();
 
@@ -10,6 +33,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
