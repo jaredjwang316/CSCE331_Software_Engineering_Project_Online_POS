ecfc90c9da8a6293532d02fcfaee77d0de4ed9fc
Mrinal <mrinal23@tamu.edu>
2023-11-07 21:53:38 -0600

    Made code more efficient

--- pyscript/script.py
+++ pyscript/script.py
@@ -9,7 +9,7 @@ response = requests.get(url)
 if response.status_code == 200:
     soup = BeautifulSoup(response.content, 'html.parser')
     items = soup.find_all('img', class_='gc-itemimage')
-    x = 0
+    x = 02
     output = ""
     for item in items:
         x += 1
