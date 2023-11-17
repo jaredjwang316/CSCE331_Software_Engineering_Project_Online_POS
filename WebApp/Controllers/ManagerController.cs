e3fd21b2189bf6fda21a8bd34d6799340b263768
cfkelley <cfkelley@tamu.edu>
2023-11-17 00:20:03 -0600

    Inventory saves

--- WebApp/Controllers/ManagerController.cs
+++ WebApp/Controllers/ManagerController.cs
@@ -33,4 +33,44 @@ public class ManagerController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+    // fetch('/Manager/SaveProducts', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify(data)
+    //     }).then(function(response) {
+    //         console.log(response);
+    //     }).catch(function(error) {
+    //         console.log(error);
+    //     });
+    [HttpPost]
+    public IActionResult SaveProducts([FromBody]List<Product> data) {
+        UnitOfWork unit = new(Config.AWS_DB_NAME);
+        foreach (Product product in data) {
+            Product initial_product = unit.Get<Product>(product.Id);
+            product.IsDrink = initial_product.IsDrink;
+            product.Hidden = initial_product.Hidden;
+            product.IsOption = initial_product.IsOption;
+            product.ImgUrl = initial_product.ImgUrl;
+            unit.Update<Product>(initial_product, product);
+        }
+
+        unit.CloseConnection();
+
+        return Ok();
+    }
+
+    public IActionResult SaveInventory([FromBody]List<Inventory> data) {
+        UnitOfWork unit = new(Config.AWS_DB_NAME);
+        foreach (Inventory inventory in data) {
+            Inventory initial_inventory = unit.Get<Inventory>(inventory.Id);
+            unit.Update<Inventory>(initial_inventory, inventory);
+        }
+
+        unit.CloseConnection();
+
+        return Ok();
+    }
 }
